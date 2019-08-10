4f651a08ba51050fb677bd4be7f22d2471e5dd28
Martin Milius <martin.milius@gmail.com>
2019-08-10 14:21:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  i = 0
-  while i < array.length do
-    puts array[i]
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
     counter += 1
 end
 end
